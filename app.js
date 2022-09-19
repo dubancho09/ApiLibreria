497d841e9fe3bf8f7d914e320fc350389d2e5bf5
09dubancho <duban5555@gmail.com>
2022-09-18 23:08:07 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,35 @@
+'use strict'
+
+var express= require('express');
+var bodyParser= require('body-parser');
+var app= express();
+
+//archivos de rutas rutas 
+
+//middlewares se ejecuta antes de la acción de un controlador
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+//CORS
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
+//rutas
+app.use('/api', (res, req)=>{
+});
+
+app.get('/', (req, res)=>{
+    res.send("funciona Melaaaa");
+})
+
+
+//Exportar
+
+module.exports=app;
